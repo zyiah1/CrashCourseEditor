c049cb17b9772bd33958468d176da6290d850914
zyiah1 <justguy493@gmail.com>
2024-08-04 20:34:57 -0500

    Object importing implemented

--- Scripts/loadreworked.gd
+++ Scripts/loadreworked.gd
@@ -5,6 +5,7 @@ var content
 var loaded = false
 var rail = false
 
+var playerposition = Vector2.ZERO
 
 func _on_load_pressed():
 	fileDialog.current_path = Options.filepath
@@ -15,14 +16,246 @@ func _process(delta):
 		if Input.is_action_just_pressed("Paste"):
 			content = DisplayServer.clipboard_get()
 			$AnimationPlayer.play("transition")
-			Load("untitled")
+			LoadTest("untitled")
 
 
+var currentLayer:int = -1
+var currentMode:String = "object"
+var movingPlatforms:Array = []
+
+var scene = preload("res://Creator.tscn").instantiate()
+func LoadTest(filename):
+	$CanvasLayer.show()
+	
+	self_modulate = Color(1,1,1,0)
+	$AnimationPlayer.play("transition")
+	var timer = Timer.new()
+	add_child(timer)
+	timer.start(.05)
+	await timer.timeout;
+	content = str(content)
+	content = content.split("\n")
+	$CanvasLayer/LoadingBar.max_value = content.size()
+	var cycle = 8
+	loaded = true
+	
+	for node in get_tree().get_nodes_in_group("hide"):
+		node.hide()
+	get_parent().add_child(scene)
+	scene.get_node("Cam").enabled = true
+	scene.get_node("nonmoving/name").text = filename
+	get_parent().scale = Vector2(1,1)
+	
+	var railtype = "normal"
+	var nextid = 0
+	var overide = 0
+	var objinterested = false
+	var railinterested = false
+	var trackedinfo = []
+	var ignore = 4
+	var actualrail = null
+	while content.size() > 0:
+		var matched = false
+		match content[0]:
+			"    - Infos:":
+				currentLayer += 1
+				print(currentLayer)
+				currentMode = "object"
+				content.remove_at(0)
+				matched = true
+			"        RailInfos:":
+				currentMode = "rail"
+				content.remove_at(0)
+				matched = true
+			"          - comment: !l -1":
+				var amount = 27
+				var objectdata = []
+				while amount > 0:
+					objectdata.append(content[0])
+					content.remove_at(0)
+					amount -= 1
+				matched = true
+				var instance = getObject(objectdata[8])
+				if instance != null:
+					scene.nodes.append(instance)
+					scene.add_child(instance)
+					instance.data = objectdata
+					instance.reposition()
+					scene.connect("EXPORT", Callable(instance, "EXPORT"))
+					if objectdata[8] == "            name: Dkb_Player":
+						playerposition = instance.position
+						scene.get_node("CanvasLayer3/CanvasLayer/buttons").play("out")
+			"            - Points:":
+				var raildata = [content[0]]
+				content.remove_at(0)
+				var amount = content.find("              unit_name: Path3D")+1
+				while amount > 0:
+					raildata.append(content[0])
+					content.remove_at(0)
+					amount -= 1
+				print(raildata)
+				matched = true
+		if matched == false:
+			content.remove_at(0)
+	$CanvasLayer.hide()
+	
+	scene.get_node("Cam").paused = false
+	scene.disabledcontrolls = false
+	scene.get_node("Cam").position = playerposition
+	scene.get_node("Cam").zoom = Vector2(2.5,2.5)
+	scene.get_node("Cam").toggleUI()
 
 
+func getRail(Railname:String)->Node:
+	var Railscene = preload("res://bridge.tscn").instantiate()
+	if Railname == "              param0: 2900.00000" or Railname == "              param0: 4900.00000":
+		var nextid = -1
+		var cycle = -1
+		for line in content: #get next rail if the first is moving
+			cycle += 1
+			if line.begins_with("              param0:"):
+				if nextid == -1:
+					nextid = 0
+				if nextid == 0:
+					nextid = cycle
+		match content[nextid]:
+			"              param0: 2141.00000":
+				Railscene = preload("res://R.tscn").instantiate()
+			"              param0: 2392.00000":
+				Railscene = preload("res://EndMove.tscn").instantiate()
+			"              param0: 2200.00000":
+				Railscene = preload("res://Auto.tscn").instantiate()
+			"              param0: 2300.00000":
+				Railscene = preload("res://Auto.tscn").instantiate()
+			"              param0: 2000.00000":
+				Railscene = preload("res://Auto.tscn").instantiate()
+			"              param0: 4300.00000":
+				Railscene = preload("res://Auto.tscn").instantiate()
+			"              param0: 2110.00000":
+				Railscene = preload("res://lcrankrail.tscn").instantiate()
+			"              param0: 2111.00000":
+				Railscene = preload("res://RCrankRail.tscn").instantiate()
+			"              param0: 2150.00000":
+				Railscene = preload("res://fan.tscn").instantiate()
+			"              param0: 2140.00000":
+				Railscene = preload("res://L.tscn").instantiate()
+	
+	match Railname:
+		"              param0: 0.00000":
+			Railscene = preload("res://bridge.tscn").instantiate()
+			Railscene.invisible = true
+		"              param0: 1200.00000":
+			Railscene = preload("res://bridge.tscn").instantiate()
+			Railscene.color = Color(.13,.58,.87,1)
+		"              param0: 5100.00000":
+			Railscene = preload("res://musicrail.tscn").instantiate()
+		"              param0: 6000.00000" :
+			Railscene = preload("res://musicrail.tscn").instantiate()
+		"              param0: 3110.00000":
+			Railscene = preload("res://Lspin.tscn").instantiate()
+		"              param0: 3010.00000":
+			Railscene = preload("res://Lspin.tscn").instantiate()
+		"              param0: 3111.00000":
+			Railscene = preload("res://Rspin.tscn").instantiate()
+		"              param0: 3011.00000":
+			Railscene = preload("res://Rspin.tscn").instantiate()
+		"              param0: 3140.00000":
+			Railscene = preload("res://Lpivot.tscn").instantiate()
+		"              param0: 3150.00000":
+			Railscene = preload("res://Lpivot.tscn").instantiate()
+		"              param0: 3300.00000":
+			Railscene = preload("res://Apivot.tscn").instantiate()
+		"              param0: 3200.00000":
+			Railscene = preload("res://Apivot.tscn").instantiate()
+		"              param0: 3423.00000":
+			Railscene = preload("res://Apivot.tscn").instantiate()
+		"              param0: 3322.00000":
+			Railscene = preload("res://Apivot.tscn").instantiate()
+		"              param0: 3392.00000":
+			Railscene = preload("res://Endpivot.tscn").instantiate()
+		"              param0: 3141.00000":
+			Railscene = preload("res://Rpivot.tscn").instantiate()
+		"              param0: 3112.00000":
+			Railscene = preload("res://LSpivit.tscn").instantiate()
+		"              param0: 3113.00000":
+			Railscene = preload("res://RSpivit.tscn").instantiate()
+	return Railscene
 
-
-
+func getObject(Objectname:String) -> Node:
+	var Objectscene = preload("res://banana.tscn").instantiate()
+	match Objectname:
+		"            name: Dkb_BlackBoard01":
+			return
+		"            name: Dkb_Player":
+			var player = preload("res://player.tscn").instantiate()
+			Objectscene = player
+			scene.stored = player
+		"            name: Dkb_CheckPoint":
+			if content[9].begins_with("            param0: 2.00000"):
+				Objectscene = preload("res://finalcheckpoint.tscn").instantiate()
+			else:
+				Objectscene = preload("res://checkpoint.tscn").instantiate()
+				if content[9].begins_with("            param0: 1"):
+					Objectscene.changetofirst()
+			if content[24] == "            scale_x: -1.00000":
+				Objectscene.flip_h = true
+		"            name: Dkb_Banana":
+			Objectscene = preload("res://banana.tscn").instantiate()
+		"            name: Dkb_ChalkEntrance":
+			Objectscene = preload("res://door.tscn").instantiate()
+		"            name: Dkb_ChalkRainbow":
+			Objectscene = preload("res://door.tscn").instantiate()
+			Objectscene.texture = preload("res://rainbow.png")
+			Objectscene.offset.y = 0
+		"            name: Dkb_OneUpItem":
+			Objectscene = preload("res://1up.tscn").instantiate()
+		"            name: Dkb_Coin":
+			Objectscene = preload("res://coin.tscn").instantiate()
+		"            name: Dkb_ChalkDonkey":
+			Objectscene = preload("res://dk.tscn").instantiate()
+		"            name: Dkb_ChalkPauline":
+			Objectscene = preload("res://pauline.tscn").instantiate()
+		"            name: Dkb_ChalkBag":
+			Objectscene = preload("res://purse.tscn").instantiate()
+		"            name: Dkb_ChalkYajirushi_Kaiten":
+			Objectscene = preload("res://Arrow.tscn").instantiate()
+			Objectscene.type = "rotate"
+		"            name: Dkb_ChalkYajirushi_Arrow":
+			Objectscene = preload("res://Arrow.tscn").instantiate()
+			Objectscene.type = "big"
+		"            name: Dkb_ChalkYajirushi_45":
+			Objectscene = preload("res://Arrow.tscn").instantiate()
+			Objectscene.type = "45"
+		"            name: Dkb_ChalkYajirushi_90":
+			Objectscene = preload("res://Arrow.tscn").instantiate()
+			Objectscene.type = "90"
+		"            name: Dkb_ChalkYajirushi_180":
+			Objectscene = preload("res://Arrow.tscn").instantiate()
+			Objectscene.type = "180"
+		"            name: Dkb_ChalkYajirushi_00":
+			Objectscene = preload("res://Arrow.tscn").instantiate()
+		"            name: Dkb_ChalkUmbrella":
+			Objectscene = preload("res://hammer.tscn").instantiate()
+		"            name: Dkb_ChalkLadder":
+			Objectscene = preload("res://ladder.tscn").instantiate()
+			Objectscene.loading = true
+			
+			match int(content[9].lstrip("            param0: ")):
+				1:
+					Objectscene.texture = preload("res://ladder1.png")
+				2:
+					Objectscene.texture = preload("res://ladder2.png")
+				#3:
+					
+				4:
+					Objectscene.texture = preload("res://ladder4.png")
+				5:
+					Objectscene.texture = preload("res://ladder5.png")
+				6:
+					Objectscene.texture = preload("res://ladder6.png")
+		"            name: Dkb_ChalkBarrel":
+			Objectscene = preload("res://barrel.tscn").instantiate()
+	return Objectscene
 
 func Load(filename):
 	$CanvasLayer.show()
@@ -434,5 +667,5 @@ func _on_FileDialog_file_selected(path):
 	content = file.get_as_text()
 	
 	file.close()
-	Load(filename)
+	LoadTest(filename)
 
