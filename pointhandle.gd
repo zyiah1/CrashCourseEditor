ca541ca18d3b53c324ce64e1ad413b01337e6473
zyiah1 <justguy493@gmail.com>
2025-10-17 12:35:59 -0500

    animation 4 the grabbers

--- pointhandle.gd
+++ pointhandle.gd
@@ -8,3 +8,11 @@ func _process(delta):
 		global_position = get_global_mouse_position()+Vector2(-7,-7)
 	if rail.locked:
 		queue_free()
+
+
+func _on_mouse_entered():
+	$AnimationPlayer.play("mousein")
+
+
+func _on_mouse_exited():
+	$AnimationPlayer.play("mouseout")
